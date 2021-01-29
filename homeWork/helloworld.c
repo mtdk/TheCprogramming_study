0f90f46b8207eb62a864b25be728476303379bb8
mtdk <ruixiangxie@outlook.com>
2021-01-29 16:19:20 +0800

    202101291619

---
+++ homeWork/helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	printf("Hello World!\n");
+	
+	printf("Enter your name:");
+	scanf("%c",&a);
+	
+	printf("Hello %c",a);
+}
---
+++ pun.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("To C, or not to c: that is the question.");
+	
+	return 0;
+}
